38a1e3bf23eb3eccf798af94df53402e93b0d9ca
Michi Lo <mitchylo8228@gmail.com>
2018-10-13 11:19:25 +0800

    update

--- Script/WorkshopOne.js
+++ Script/WorkshopOne.js
@@ -2,7 +2,7 @@ function show_topic() {
   var x = document.getElementById("demo");
   var my_name = prompt("Enter your name");
   x.style.fontSize = "25px";
-  x.style.color = "red";
+  x.style.color = "beige";
   document.getElementById("demo").innerHTML = "My name is " + my_name;
 }
 
